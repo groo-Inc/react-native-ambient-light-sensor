b031672ea76daf8bf36d821b3310a7421dcf250a
DongHee Kang <kanglpmg@gmail.com>
2024-04-12 15:57:49 +0900

    [iOS] iOS support (#2)
    * feat: Add RCTEventEmitter setting
    
    * feat: Add logic to AmbientLightSensor
    
    * fix: typo
    
    * fix: typo
    
    * fix: typo

--- ios/AmbientLightSensor.swift
+++ ios/AmbientLightSensor.swift
@@ -1,8 +1,78 @@
-@objc(AmbientLightSensor)
-class AmbientLightSensor: NSObject {
+import Foundation
+import UIKit
+import AVFoundation
+import React
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+@objc(AmbientLightSensor)
+class AmbientLightSensor: RCTEventEmitter, AVCaptureVideoDataOutputSampleBufferDelegate {
+    let queue = DispatchQueue(label: "AmbientLightSensor")
+    let eventName = "LightSensor"
+
+    private let captureSession = AVCaptureSession()
+    private let videoOutput = AVCaptureVideoDataOutput()
+
+    private var isSetup = false
+    private var isObserving = false
+
+    override init() {
+        super.init()
+
+    }
+
+    override func supportedEvents() -> [String]! {
+        return ["LightSensor"]
+    }
+    
+    @objc func hasLightSensor(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
+        resolve(true)
+    }
+    
+    @objc func startLightSensor() -> Void {
+        isObserving = true;
+
+        if (!isSetup) {
+          isSetup = true
+          setupCaptureSession();
+        }
+    }
+    
+    @objc func stopLightSensor() -> Void {
+        isObserving = false
+    }
+
+    // Private methods
+
+    func setupCaptureSession() {
+        guard let camera = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .front).devices.first else { return }
+        
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            captureSession.addInput(cameraInput)
+            
+            captureSession.addOutput(videoOutput)
+            videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+            
+            captureSession.startRunning()
+        } catch {
+            print("Error setting up capture session: \(error.localizedDescription)")
+        }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        if let metadata = CMGetAttachment(sampleBuffer, key: kCGImagePropertyExifDictionary, attachmentModeOut: nil) {
+            if let FNumber = metadata["FNumber"] as? Double, let ExposureTime = metadata["ExposureTime"] as? Double, let ISOSpeedRatingsArray = metadata["ISOSpeedRatings"] as? [Double] {
+                
+                let CalibrationConstant = 1000.0
+                
+                let ISOSpeedRatings = ISOSpeedRatingsArray[0]
+                let luminosity: Double = (CalibrationConstant * FNumber * FNumber) / (ExposureTime * ISOSpeedRatings)
+                
+                if (isObserving) {
+                  sendEvent(withName: eventName, body: ["lightValue": luminosity])
+                }
+            }
+        }
+    }
+    
 }
